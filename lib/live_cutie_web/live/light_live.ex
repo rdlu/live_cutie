505acc8d0934c2fd7d2ee4e05c905a501a842522
Rodrigo Dlugokenski <git@dlu.one>
2022-07-01 18:24:09 -0300

    Up and down events

--- lib/live_cutie_web/live/light_live.ex
+++ lib/live_cutie_web/live/light_live.ex
@@ -17,13 +17,41 @@ defmodule LiveCutieWeb.LightLive do
           <%= @brightness %>
         </span>
       </div>
-      <button class="bg-transparent mx-1 py-2 px-4 border border-cool-gray-400 border-2 rounded-lg shadow-sm transition ease-in-out duration-150 outline-none hover:bg-cool-gray-300 disabled:opacity-50 disabled:cursor-not-allowed">
+      <button phx-click="off" class="bg-transparent mx-1 py-2 px-4 border border-cool-gray-400 border-2 rounded-lg shadow-sm transition ease-in-out duration-150 outline-none hover:bg-cool-gray-300 disabled:opacity-50 disabled:cursor-not-allowed">
         <img src="images/light-off.svg" alt="Lights Off" class="w-10">
       </button>
-      <button class="bg-transparent mx-1 py-2 px-4 border border-cool-gray-400 border-2 rounded-lg shadow-sm transition ease-in-out duration-150 outline-none hover:bg-cool-gray-300 disabled:opacity-50 disabled:cursor-not-allowed">
+      <button phx-click="on" class="bg-transparent mx-1 py-2 px-4 border border-cool-gray-400 border-2 rounded-lg shadow-sm transition ease-in-out duration-150 outline-none hover:bg-cool-gray-300 disabled:opacity-50 disabled:cursor-not-allowed">
         <img src="images/light-on.svg" alt="Lights On" class="w-10">
+      </button>
+      <button phx-click="up" class="bg-transparent mx-1 py-2 px-4 border border-cool-gray-400 border-2 rounded-lg shadow-sm transition ease-in-out duration-150 outline-none hover:bg-cool-gray-300 disabled:opacity-50 disabled:cursor-not-allowed">
+        <img src="images/up.svg" alt="Lights Up" class="w-10">
+      </button>
+      <button phx-click="down" class="bg-transparent mx-1 py-2 px-4 border border-cool-gray-400 border-2 rounded-lg shadow-sm transition ease-in-out duration-150 outline-none hover:bg-cool-gray-300 disabled:opacity-50 disabled:cursor-not-allowed">
+        <img src="images/down.svg" alt="Lights Down" class="w-10">
       </button>
     </div>
     """
+  end
+
+  def handle_event("on", _, socket) do
+    socket = assign(socket, :brightness, 100)
+    {:noreply, socket}
+  end
+
+  def handle_event("off", _, socket) do
+    socket = assign(socket, :brightness, 0)
+    {:noreply, socket}
+  end
+
+  def handle_event("up", _, socket) do
+    socket = assign(socket, :brightness, socket.assigns.brightness + 10)
+    IO.inspect(socket)
+    {:noreply, socket}
+  end
+
+  def handle_event("down", _, socket) do
+    socket = assign(socket, :brightness, socket.assigns.brightness - 10)
+    IO.inspect(socket)
+    {:noreply, socket}
   end
 end
