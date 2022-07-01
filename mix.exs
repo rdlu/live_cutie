289eb7012a1b863b735c1ee35fdb6830e35105bc
Rodrigo Dlugokenski <git@dlu.one>
2022-07-01 13:59:24 -0300

    Added faker, number, tailwind to mix

--- mix.exs
+++ mix.exs
@@ -48,7 +48,10 @@ defmodule LiveCutie.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:faker, "~> 0.17.0"},
+      {:number, "~> 1.0"},
+      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev}
     ]
   end
 
