099a310bf63b058d853e82dcebb9209644cdd9aa
Rodrigo Dlugokenski <git@dlu.one>
2022-07-01 17:52:10 -0300

    Using embedded heex, using correct Tailwind styles

--- lib/live_cutie_web/live/light_live.ex
+++ lib/live_cutie_web/live/light_live.ex
@@ -8,18 +8,21 @@ defmodule LiveCutieWeb.LightLive do
   end
 
   def render(assigns) do
-    ~L"""
+    style = "width: #{assigns[:brightness]}%; transition: width 2s ease;"
+    ~H"""
     <h1>Front Light</h1>
     <div id="light" class="max-w-xl mx-auto text-center">
       <div class="flex h-12 overflow-hidden text-base bg-cool-gray-300 rounded-lg mb-8">
-        <span class="flex flex-col justify-center text-cool-gray-900 text-center whitespace-nowrap bg-yellow-300 font-bold" style="width: <%= @brightness %>%; transition: width 2s ease;">
+        <span class="flex flex-col justify-center text-cool-gray-900 text-center whitespace-nowrap bg-yellow-300 font-bold" style={style}>
           <%= @brightness %>
         </span>
       </div>
       <button class="bg-transparent mx-1 py-2 px-4 border border-cool-gray-400 border-2 rounded-lg shadow-sm transition ease-in-out duration-150 outline-none hover:bg-cool-gray-300 disabled:opacity-50 disabled:cursor-not-allowed">
         <img src="images/light-off.svg" alt="Lights Off" class="w-10">
       </button>
-      <button><img src="images/light-on.svg" alt="Lights On" class="w-10"></button>
+      <button class="bg-transparent mx-1 py-2 px-4 border border-cool-gray-400 border-2 rounded-lg shadow-sm transition ease-in-out duration-150 outline-none hover:bg-cool-gray-300 disabled:opacity-50 disabled:cursor-not-allowed">
+        <img src="images/light-on.svg" alt="Lights On" class="w-10">
+      </button>
     </div>
     """
   end
