577591cea8cbfecc21fe411c7f2d6e584e8b8d98
Rodrigo Dlugokenski <git@dlu.one>
2022-07-04 09:01:34 -0300

    Disabling buttons

--- lib/live_cutie_web/live/light_live.ex
+++ lib/live_cutie_web/live/light_live.ex
@@ -8,26 +8,25 @@ defmodule LiveCutieWeb.LightLive do
   end
 
   def render(assigns) do
-    style = "width: #{assigns[:brightness]}%; transition: width 2s ease;"
     ~H"""
     <h1>Lights Control</h1>
     <div id="light" class="max-w-xl mx-auto text-center">
       <div class="flex h-12 overflow-hidden text-base bg-slate-300 rounded-lg mb-8 border-2 border-slate-800">
-        <span class="flex flex-col justify-center text-slate-900 text-center whitespace-nowrap bg-gradient-to-r from-indigo-500 to-cyan-500 font-bold" style={style}>
+        <span class="flex flex-col justify-center text-slate-900 text-center whitespace-nowrap bg-gradient-to-r from-indigo-500 to-cyan-500 font-bold" style={"width: #{@brightness}%; transition: width 2s ease;"}>
           <%= @brightness %>
         </span>
       </div>
-      <button phx-click="up" class="bg-lime-300 mx-1 py-2 px-4 border border-slate-400 border-2 rounded-lg shadow-sm transition ease-in-out duration-150 outline-none hover:bg-slate-300 disabled:opacity-50 disabled:cursor-not-allowed">
-        <img src="images/arrow-circle-up-solid.svg" alt="Lights Up" class="w-10">
+      <button phx-click="on" disabled={@brightness >= 100} class="bg-yellow-300 mx-1 py-2 px-4 border border-slate-400 border-2 rounded-lg shadow-sm transition ease-in-out duration-150 outline-none hover:bg-slate-300 disabled:opacity-50 disabled:cursor-not-allowed">
+        <img src="images/sun.svg" alt="Lights On" class="w-10">
       </button>
-      <button phx-click="down" class="bg-violet-300 mx-1 py-2 px-4 border border-slate-400 border-2 rounded-lg shadow-sm transition ease-in-out duration-150 outline-none hover:bg-slate-300 disabled:opacity-50 disabled:cursor-not-allowed">
-        <img src="images/arrow-circle-down-solid.svg" alt="Lights Down" class="w-10">
-      </button>
-      <button phx-click="off" class="bg-indigo-300 mx-1 py-2 px-4 border border-slate-400 border-2 rounded-lg shadow-sm transition ease-in-out duration-150 outline-none hover:bg-slate-300 disabled:opacity-50 disabled:cursor-not-allowed">
+      <button phx-click="off" disabled={@brightness <= 0} class="bg-indigo-300 mx-1 py-2 px-4 border border-slate-400 border-2 rounded-lg shadow-sm transition ease-in-out duration-150 outline-none hover:bg-slate-300 disabled:opacity-50 disabled:cursor-not-allowed">
         <img src="images/moon.svg" alt="Lights Off" class="w-10">
       </button>
-      <button phx-click="on" class="bg-yellow-300 mx-1 py-2 px-4 border border-slate-400 border-2 rounded-lg shadow-sm transition ease-in-out duration-150 outline-none hover:bg-slate-300 disabled:opacity-50 disabled:cursor-not-allowed">
-        <img src="images/sun.svg" alt="Lights On" class="w-10">
+      <button phx-click="up" disabled={@brightness >= 100} class="bg-lime-300 mx-1 py-2 px-4 border border-slate-400 border-2 rounded-lg shadow-sm transition ease-in-out duration-150 outline-none hover:bg-slate-300 disabled:opacity-50 disabled:cursor-not-allowed">
+        <img src="images/arrow-circle-up-solid.svg" alt="Lights Up" class="w-10">
+      </button>
+      <button phx-click="down" disabled={@brightness <= 0} class="bg-violet-300 mx-1 py-2 px-4 border border-slate-400 border-2 rounded-lg shadow-sm transition ease-in-out duration-150 outline-none hover:bg-slate-300 disabled:opacity-50 disabled:cursor-not-allowed">
+        <img src="images/arrow-circle-down-solid.svg" alt="Lights Down" class="w-10">
       </button>
     </div>
     """
@@ -44,13 +43,13 @@ defmodule LiveCutieWeb.LightLive do
   end
 
   def handle_event("up", _, socket) do
-    socket = assign(socket, :brightness, socket.assigns.brightness + 10)
+    socket = update(socket, :brightness, &min(&1 + 10, 100))
     IO.inspect(socket)
     {:noreply, socket}
   end
 
   def handle_event("down", _, socket) do
-    socket = assign(socket, :brightness, socket.assigns.brightness - 10)
+    socket = update(socket, :brightness, &max(&1 - 10, 0))
     IO.inspect(socket)
     {:noreply, socket}
   end
