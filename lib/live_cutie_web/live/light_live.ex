b49a8a448dd45b9c20865f60cd7daa70c951b7d1
Rodrigo Dlugokenski <git@dlu.one>
2022-07-04 08:40:00 -0300

    Icons and default tailwind colors

--- lib/live_cutie_web/live/light_live.ex
+++ lib/live_cutie_web/live/light_live.ex
@@ -10,24 +10,24 @@ defmodule LiveCutieWeb.LightLive do
   def render(assigns) do
     style = "width: #{assigns[:brightness]}%; transition: width 2s ease;"
     ~H"""
-    <h1>Front Light</h1>
+    <h1>Lights Control</h1>
     <div id="light" class="max-w-xl mx-auto text-center">
-      <div class="flex h-12 overflow-hidden text-base bg-cool-gray-300 rounded-lg mb-8">
-        <span class="flex flex-col justify-center text-cool-gray-900 text-center whitespace-nowrap bg-yellow-300 font-bold" style={style}>
+      <div class="flex h-12 overflow-hidden text-base bg-slate-300 rounded-lg mb-8 border-2 border-slate-800">
+        <span class="flex flex-col justify-center text-slate-900 text-center whitespace-nowrap bg-gradient-to-r from-indigo-500 to-cyan-500 font-bold" style={style}>
           <%= @brightness %>
         </span>
       </div>
-      <button phx-click="off" class="bg-transparent mx-1 py-2 px-4 border border-cool-gray-400 border-2 rounded-lg shadow-sm transition ease-in-out duration-150 outline-none hover:bg-cool-gray-300 disabled:opacity-50 disabled:cursor-not-allowed">
-        <img src="images/light-off.svg" alt="Lights Off" class="w-10">
+      <button phx-click="up" class="bg-lime-300 mx-1 py-2 px-4 border border-slate-400 border-2 rounded-lg shadow-sm transition ease-in-out duration-150 outline-none hover:bg-slate-300 disabled:opacity-50 disabled:cursor-not-allowed">
+        <img src="images/arrow-circle-up-solid.svg" alt="Lights Up" class="w-10">
       </button>
-      <button phx-click="on" class="bg-transparent mx-1 py-2 px-4 border border-cool-gray-400 border-2 rounded-lg shadow-sm transition ease-in-out duration-150 outline-none hover:bg-cool-gray-300 disabled:opacity-50 disabled:cursor-not-allowed">
-        <img src="images/light-on.svg" alt="Lights On" class="w-10">
+      <button phx-click="down" class="bg-violet-300 mx-1 py-2 px-4 border border-slate-400 border-2 rounded-lg shadow-sm transition ease-in-out duration-150 outline-none hover:bg-slate-300 disabled:opacity-50 disabled:cursor-not-allowed">
+        <img src="images/arrow-circle-down-solid.svg" alt="Lights Down" class="w-10">
       </button>
-      <button phx-click="up" class="bg-transparent mx-1 py-2 px-4 border border-cool-gray-400 border-2 rounded-lg shadow-sm transition ease-in-out duration-150 outline-none hover:bg-cool-gray-300 disabled:opacity-50 disabled:cursor-not-allowed">
-        <img src="images/up.svg" alt="Lights Up" class="w-10">
+      <button phx-click="off" class="bg-indigo-300 mx-1 py-2 px-4 border border-slate-400 border-2 rounded-lg shadow-sm transition ease-in-out duration-150 outline-none hover:bg-slate-300 disabled:opacity-50 disabled:cursor-not-allowed">
+        <img src="images/moon.svg" alt="Lights Off" class="w-10">
       </button>
-      <button phx-click="down" class="bg-transparent mx-1 py-2 px-4 border border-cool-gray-400 border-2 rounded-lg shadow-sm transition ease-in-out duration-150 outline-none hover:bg-cool-gray-300 disabled:opacity-50 disabled:cursor-not-allowed">
-        <img src="images/down.svg" alt="Lights Down" class="w-10">
+      <button phx-click="on" class="bg-yellow-300 mx-1 py-2 px-4 border border-slate-400 border-2 rounded-lg shadow-sm transition ease-in-out duration-150 outline-none hover:bg-slate-300 disabled:opacity-50 disabled:cursor-not-allowed">
+        <img src="images/sun.svg" alt="Lights On" class="w-10">
       </button>
     </div>
     """
