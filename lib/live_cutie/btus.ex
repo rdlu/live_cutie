110df1d3cce8fafc0c39f6d227f958e02ea054b1
Rodrigo Dlugokenski <git@dlu.one>
2022-07-04 11:20:14 -0300

    Slider with Timex

--- lib/live_cutie/btus.ex
+++ lib/live_cutie/btus.ex
@@ -8,11 +8,18 @@ defmodule LiveCutie.Btus do
   }
 
   def calculate(level) do
-    if level < 3 do
-      level * 4000
-    else
-      8000 + (level - 2) * 2000
-    end
+    expiration_time = autoshutdown_timer(level)
+    remaining = time_remaining(expiration_time)
+    btus = calculate_btus(level)
+
+    %{level: level,
+     btus: btus,
+     time_remaining: remaining,
+     expiration_time: expiration_time}
+  end
+
+  def time_remaining(expiration_time) do
+    DateTime.diff(expiration_time, Timex.now())
   end
 
   def human_level(level) when is_map_key(@human_level, level) do
@@ -20,4 +27,20 @@ defmodule LiveCutie.Btus do
   end
 
   def human_level(_level), do: "Unknown"
+
+  defp calculate_btus(level) do
+    if level < 3 do
+      level * 4000
+    else
+      8000 + (level - 2) * 2000
+    end
+  end
+
+  defp autoshutdown_timer(level) do
+    if level < 3 do
+      Timex.shift(Timex.now(), hours: 4)
+    else
+      Timex.shift(Timex.now(), hours: 2)
+    end
+  end
 end
--- lib/live_cutie_web/live/slider_live.ex
+++ lib/live_cutie_web/live/slider_live.ex
@@ -4,8 +4,13 @@ defmodule LiveCutieWeb.SliderLive do
   alias LiveCutie.Btus
 
   def mount(_params, _session, socket) do
+    if connected?(socket) do
+      :timer.send_interval(1000, self(), :self_refresh)
+    end
+
     level = 2
-    socket = assign(socket, level: level, btus: Btus.calculate(level))
+    data = Btus.calculate(level)
+    socket = assign(socket, data)
     {:ok, socket}
   end
 
@@ -29,14 +34,33 @@ defmodule LiveCutieWeb.SliderLive do
           </div>
         </div>
       </div>
+      <p class="m-4 font-semibold text-indigo-800">
+        <%= if @time_remaining > 0 do %>
+          <%= format_time(@time_remaining) %> left before shutdown
+        <% else %>
+          Expired!
+        <% end %>
+      </p>
     </div>
     """
   end
 
   def handle_event("update", %{"level" => level}, socket) do
     level = String.to_integer(level)
-    btus = Btus.calculate(level)
-    socket = assign(socket, level: level, btus: btus)
+    data = Btus.calculate(level)
+    socket = assign(socket, data)
     {:noreply, socket}
+  end
+
+  def handle_info(:self_refresh, socket) do
+    expiration_time = socket.assigns.expiration_time
+    socket = assign(socket, time_remaining: Btus.time_remaining(expiration_time))
+    {:noreply, socket}
+  end
+
+  defp format_time(time) do
+    time
+    |> Timex.Duration.from_seconds()
+    |> Timex.format_duration(:humanized)
   end
 end
