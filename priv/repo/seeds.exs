8b5378dd90cf26227378653a4ea7fdcb378da3b3
Rodrigo Dlugokenski <git@dlu.one>
2022-07-05 10:50:35 -0300

    Petz Adding some data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,53 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias LiveCutie.Repo
+alias LiveCutie.Pets.Pet
+
+%Pet{
+  name: "Fido",
+  species: "dog",
+  cuteness: "4",
+  image: "images/petz/fido.jpg"
+}
+|> Repo.insert!()
+
+%Pet{
+  name: "Brad",
+  species: "dog",
+  cuteness: "5",
+  image: "images/petz/brad.jpg"
+}
+|> Repo.insert!()
+
+%Pet{
+  name: "Fluffy",
+  species: "cat",
+  cuteness: "5",
+  image: "images/petz/fluffy.jpg"
+}
+|> Repo.insert!()
+
+%Pet{
+  name: "Lily",
+  species: "cat",
+  cuteness: "4",
+  image: "images/petz/lily.jpg"
+}
+|> Repo.insert!()
+
+%Pet{
+  name: "Bella",
+  species: "cat",
+  cuteness: "5",
+  image: "images/petz/bella.jpg"
+}
+|> Repo.insert!()
+
+%Pet{
+  name: "Polly",
+  species: "bird",
+  cuteness: "4",
+  image: "images/petz/polly.jpg"
+}
+|> Repo.insert!()
