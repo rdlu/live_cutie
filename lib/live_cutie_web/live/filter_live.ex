2906827b84aa261d25f67fca6912a00f98484d99
Rodrigo Dlugokenski <git@dlu.one>
2022-07-06 08:53:10 -0300

    Live Views: breaking heex templates into files

--- lib/live_cutie_web/live/filter_live.ex
+++ lib/live_cutie_web/live/filter_live.ex
@@ -8,64 +8,6 @@ defmodule LiveCutieWeb.FilterLive do
     {:ok, socket}
   end
 
-  def render(assigns) do
-    ~H"""
-    <h1>Cute Pets</h1>
-    <div id="filter">
-      <form id="max-w-xl mx-auto mb-4" phx-change="filter">
-        <div class="filters flex items-baseline justify-around">
-          <select
-            name="species"
-            class="appearance-none w-1/3 bg-slate-200 border border-slate-400 text-slate-700 py-3 px-4 rounded-lg leading-tight font-semibold cursor-pointer"
-          >
-            <%= options_for_select(species_options(), @species) %>
-          </select>
-          <div class="cuteness flex">
-            <input type="hidden" name="cuteness[]" value="" />
-            <%= for i <- 3..5  do %>
-              <%= cuteness_checkbox(cuteness: i, checked: to_string(i) in @cuteness) %>
-            <% end %>
-          </div>
-          <button
-            type="reset"
-            phx-click="clear"
-            class=" bg-slate-200 border border-slate-400 text-slate-700 py-3 px-4 rounded-lg leading-tight font-semibold hover:bg-slate-400 hover:cursor-pointer"
-          >
-            Clear All
-          </button>
-        </div>
-      </form>
-      <div class="pets flex flex-wrap justify-center">
-        <%= for pet <- @pets do %>
-          <div class="card flex flex-col m-6 max-w-sm rounded bg-slate-100 overflow-hidden shadow-lg place-content-around">
-            <img src={pet.image} />
-            <div class="content px-6 py-4 mt-2 flex justify-between">
-              <div class="name pb-3 text-center text-slate-900 font-bold text-xl">
-                <%= pet.name %>
-              </div>
-              <div class="cuteness text-slate-700 font-semibold text-xl">
-                <div class="inline-flex py-3 px-4">
-                  <%= for _ <- 1..String.to_integer(pet.cuteness) do %>
-                    <img src="images/heart-solid.svg" alt="Heart" class="w-3 h-3" />
-                  <% end %>
-                </div>
-              </div>
-              <div class="species uppercase inline-block self-center bg-slate-300 rounded-full px-2 py-1 text-sm font-semibold text-slate-700">
-                <%= pet.species %>
-              </div>
-            </div>
-          </div>
-        <% end %>
-        <%= if Enum.empty?(@pets) do %>
-          <div class="p-20 m-20 font-semibold text-red-500 rounded bg-slate-100 overflow-hidden shadow-lg">
-            Nothing found here. Please change or remove some filters.
-          </div>
-        <% end %>
-      </div>
-    </div>
-    """
-  end
-
   def handle_event("filter", %{"species" => species, "cuteness" => cuteness}, socket) do
     params = [species: species, cuteness: cuteness]
     pets = Pets.list_pets(params)
--- lib/live_cutie_web/live/slider_live.ex
+++ lib/live_cutie_web/live/slider_live.ex
@@ -14,37 +14,6 @@ defmodule LiveCutieWeb.SliderLive do
     {:ok, socket}
   end
 
-  def render(assigns) do
-    ~H"""
-    <h1>Slider Control</h1>
-    <div id="slider" class="max-w-lg mx-auto text-center">
-      <div class="card bg-white shadow rounded-lg shadow-lg">
-        <div class="content p-6">
-          <div class="level inline-flex items-center mb-8">
-            <img src="images/fan-solid.svg" class="w-10 pr-2" />
-            <span class="text-xl font-semibold text-slate-700">
-              The current level is <strong><%= Btus.human_level(@level) %></strong>.
-            </span>
-          </div>
-          <form phx-change="update">
-            <input type="range" min="0" max="4" name="level" value="2" phx-debounce="250" />
-          </form>
-          <div class="btus text-2xl leading-none text-slate-900 mt-4">
-            <strong><%= @btus %></strong> BTU/h
-          </div>
-        </div>
-      </div>
-      <p class="m-4 font-semibold text-indigo-800">
-        <%= if @time_remaining > 0 do %>
-          <%= format_time(@time_remaining) %> left before shutdown
-        <% else %>
-          Expired!
-        <% end %>
-      </p>
-    </div>
-    """
-  end
-
   def handle_event("update", %{"level" => level}, socket) do
     level = String.to_integer(level)
     data = Btus.calculate(level)
