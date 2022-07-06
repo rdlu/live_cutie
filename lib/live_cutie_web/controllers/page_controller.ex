8ba827ca8b993324f5d363ff8b09ebdaef8ffb91
Rodrigo Dlugokenski <git@dlu.one>
2022-07-06 14:26:29 -0300

    Home, TOC, back button

--- lib/live_cutie_web/controllers/page_controller.ex
+++ lib/live_cutie_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule LiveCutieWeb.PageController do
   use LiveCutieWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html", socket: conn)
   end
 end
